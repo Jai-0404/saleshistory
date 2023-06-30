256b370c52311a8cd68470972e660398968fce96
MSSNVGPA <MSSNVGPA@INLEN8520041992.corp.capgemini.com>
2023-06-30 17:52:33 +0530

    EAdded annotations

--- src/main/java/com/cg/sales/entity/Customer.java
+++ src/main/java/com/cg/sales/entity/Customer.java
@@ -2,7 +2,11 @@ package com.cg.sales.entity;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -20,27 +24,51 @@ import lombok.ToString;
 @Table(name="customers")
 public class Customer {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int custId;
+	@Column
 	private String custFirstName;
+	@Column
 	private String custLastName;
+	@Column
 	private char custGender;
+	@Column
 	private int custYearOfBirth;
+	@Column
 	private String custMartialStatus;
+	@Column
 	private String custStreetAddress;
+	@Column
 	private String custPostalCode;
+	@Column
 	private String custCity;
+	@Column
 	private int custCityId;
+	@Column
 	private String custStateProvince;
+	@Column
 	private int custStateId;
+	@Column
 	private int countryId;
+	@Column
 	private String custMainPhoneINT;
+	@Column
 	private String custIncomeLevel;
+	@Column
 	private int custCreditLimit;
+	@Column
 	private String custEmail;
+	@Column
 	private String custTotal;
+	@Column
 	private int custTotalId;
+	@Column
 	private int custSrcId;
+	@Column
 	private Date custEffFrom;
+	@Column
 	private Date custEffTo;
+	@Column
 	private String custValid;
 }
