919b5ca2aa95c0b4bed3b62614c7facffa9f68d2
katurohi <katurohi@INLEN8520041990.corp.capgemini.com>
2023-06-30 20:04:41 +0530

    Added sales

---
+++ src/main/java/com/cg/sales/entity/Sales.java
@@ -0,0 +1,17 @@
+package com.cg.sales.entity;
+
+import java.util.Date;
+
+public class Sales {
+	
+	private int prodId;
+	private int custId;
+	private Date timeId;
+	private int channelId;
+	private int promoId;
+	private int quantitySold;
+	private double amountSold;
+	
+	
+
+}
