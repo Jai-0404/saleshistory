eb8a1b9cf4adbbfe1ea3f62c05f72feb90573042
mssnvgpa <mssnvgpa@INLEN8520041992.corp.capgemini.com>
2023-07-01 11:15:08 +0530

    Added new entities

--- src/main/java/com/cg/sales/entity/Cost.java
+++ src/main/java/com/cg/sales/entity/Cost.java
@@ -4,8 +4,6 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -21,18 +19,18 @@ import lombok.ToString;
 @ToString
 @Entity
 @Table(name="costs")
-public class Costs {
+public class Cost {
 
-	@Id
+	@Column(name="prod_id")
 	private int prodId;
-	@Column
+	@Column(name="time_id")
 	private int timeId;
-	@Column
+	@Column(name="promo_id")
 	private Date promoId;
-	@Column
+	@Column(name="channel_id")
 	private int channelId;
-	@Column
+	@Column(name="unit_cost")
 	private int unitCost;
-	@Column
+	@Column(name="unit_price")
 	private double unitPrice;
 }
