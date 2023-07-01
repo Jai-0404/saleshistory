ea063b4761fb11592a5b4da7d869de9ef1090fcc
mssnvgpa <mssnvgpa@INLEN8520041992.corp.capgemini.com>
2023-07-01 10:23:04 +0530

    Added Swagger annotation

---
+++ src/main/java/com/cg/sales/SalesHistoryApplication.java
@@ -0,0 +1,16 @@
+package com.cg.sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+ 
+@SpringBootApplication
+@EnableSwagger2
+public class SalesHistoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesHistoryApplication.class, args);
+    }
+}
